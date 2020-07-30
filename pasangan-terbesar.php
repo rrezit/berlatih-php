5303c031f33702f9423477ca233f544a4b9cec69
root <rezarasyid.reza@gmail.com>
2020-07-30 11:07:39 +0700

    proses

--- pasangan-terbesar.php
+++ pasangan-terbesar.php
@@ -7,14 +7,18 @@ function pasangan_terbesar($angka){
     // 3. pasangan angka pertama di set sebagai terbesar
     // 4. selama looping kalau ketemu angka yang lebih besar, terbesarnya di update dengan angka
     // 5. return angka terbesar
-    
+    for($i = 0; $i < strlen($angka); $i++){
+        $tampung = substr($angka, $i, $i+1);
+        echo "$tampung ";
+    }
 }
 
+
 // TEST CASES
-echo pasangan_terbesar(641573); // 73
-echo pasangan_terbesar(12783456); // 83
-echo pasangan_terbesar(910233); // 91
-echo pasangan_terbesar(71856421); // 85
-echo pasangan_terbesar(79918293); // 99
+echo "<br>" . pasangan_terbesar(641573); // 73
+echo "<br>" . pasangan_terbesar(12783456); // 83
+echo "<br>" . pasangan_terbesar(910233); // 91
+echo "<br>" . pasangan_terbesar(71856421); // 85
+echo "<br>" . pasangan_terbesar(79918293); // 99
 
 ?>
