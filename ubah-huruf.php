c8994403e975e37e9a983a8557878206968ee3bc
root <rezarasyid.reza@gmail.com>
2020-07-30 09:15:37 +0700

    ok

--- ubah-huruf.php
+++ ubah-huruf.php
@@ -1,13 +1,26 @@
 <?php
 function ubah_huruf($string){
 //kode di sini
+    // pseudocode
+    // 1. kita buat manual dulu perpustakaan abjad alphabet
+    // 2. looping per karakter pada parameter string
+    // 3. tampung ke variabel penampung: karakter pada posisi $i + 1 di alphabetnya
+    // 4. return penampung
+
+    $abjad = "abcdefghijklmnopqrstuvwxyz";
+    $output = "";
+    for($i = 0; $i < strlen($string); $i++){
+        $position = strrpos($abjad, $string[$i]);
+        $output .= substr($abjad, $position + 1, 1);
+    }
+    return $output;
 }
 
 // TEST CASES
 echo ubah_huruf('wow'); // xpx
-echo ubah_huruf('developer'); // efwfmpqfs
-echo ubah_huruf('laravel'); // mbsbwfm
-echo ubah_huruf('keren'); // lfsfo
-echo ubah_huruf('semangat'); // tfnbohbu
+echo "<br>" . ubah_huruf('developer'); // efwfmpqfs
+echo "<br>" . ubah_huruf('laravel'); // mbsbwfm
+echo "<br>" . ubah_huruf('keren'); // lfsfo
+echo "<br>" . ubah_huruf('semangat'); // tfnbohbu
 
 ?>
