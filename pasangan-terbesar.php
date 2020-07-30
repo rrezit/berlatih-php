2d5507960b166bed88b5b35993baa7628f1e3a22
root <rezarasyid.reza@gmail.com>
2020-07-30 09:16:01 +0700

    progress

--- pasangan-terbesar.php
+++ pasangan-terbesar.php
@@ -1,6 +1,13 @@
 <?php
 function pasangan_terbesar($angka){
 // kode di sini
+    // pseudocode
+    // 1. looping sebanyak karakter pada angka
+    // 2. dapatkan pasangan angka dengan metode substr
+    // 3. pasangan angka pertama di set sebagai terbesar
+    // 4. selama looping kalau ketemu angka yang lebih besar, terbesarnya di update dengan angka
+    // 5. return angka terbesar
+    
 }
 
 // TEST CASES
