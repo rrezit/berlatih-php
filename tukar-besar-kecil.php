9875453593825985e942c08baa9f554fb3fd5c12
root <rezarasyid.reza@gmail.com>
2020-07-30 11:17:00 +0700

    proses

--- tukar-besar-kecil.php
+++ tukar-besar-kecil.php
@@ -1,13 +1,18 @@
 <?php
 function tukar_besar_kecil($string){
 //kode di sini
+    $tampung = strtoupper($string);
+    $output = lcfirst($tampung);
+    // echo $output;
+    return $output;
+
 }
 
 // TEST CASES
 echo tukar_besar_kecil('Hello World'); // "hELLO wORLD"
-echo tukar_besar_kecil('I aM aLAY'); // "i Am Alay"
-echo tukar_besar_kecil('My Name is Bond!!'); // "mY nAME IS bOND!!"
-echo tukar_besar_kecil('IT sHOULD bE me'); // "it Should Be ME"
-echo tukar_besar_kecil('001-A-3-5TrdYW'); // "001-a-3-5tRDyw"
+echo "<br>" . tukar_besar_kecil('I aM aLAY'); // "i Am Alay"
+echo "<br>" . tukar_besar_kecil('My Name is Bond!!'); // "mY nAME IS bOND!!"
+echo "<br>" . tukar_besar_kecil('IT sHOULD bE me'); // "it Should Be ME"
+echo "<br>" . tukar_besar_kecil('001-A-3-5TrdYW'); // "001-a-3-5tRDyw"
 
 ?>
